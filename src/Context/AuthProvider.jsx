0bf76cc2e90aa2029df5651004b717b7feb13ad6
rashel9255 <ibrahim2000167@gmail.com>
2025-12-09 23:05:26 +0600

    feat: add user state management and sign out functionality in authprovider

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,32 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "../Context/AuthContext";
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "../Firebase/firebase.init";
 const googleProvider = new GoogleAuthProvider();
 
-const AuthProvider = ({ children }) => {
+const AuthProvider = ({ children }) => {  
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
     // Google Sign In
     const signInWithGoogle = () => {
+        setLoading(true);
         return signInWithPopup(auth, googleProvider);
     };
 
     //register with email-password
     const registerUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     };
 
     //sign in with email-password
     const signInUser = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     };
+
+    //sign out
+    const signOutUser = () => {
+        setLoading(true);
+        return signOut(auth);
+    };
+
+
+    //observe current user
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => {
+            unsubscribe();
+        };
+    }, [])
+    
 
 
 
     const authInfo = {
+        user,
+        loading,
         signInWithGoogle,
         registerUser,
         signInUser,
-
+        signOutUser,
     };
 
     return <AuthContext value={authInfo}>{children}</AuthContext>;
