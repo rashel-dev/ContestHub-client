1a7dcfcf173dc22bc0b5508c4d0cb24d97d8d89d
rashel9255 <ibrahim2000167@gmail.com>
2025-12-15 04:13:36 +0600

    feat: select winner and send neccessary data to the server

--- src/Pages/Dashboard/ContestParticipants/ContestParticipants.jsx
+++ src/Pages/Dashboard/ContestParticipants/ContestParticipants.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
-import { useParams } from 'react-router';
-import useAxiosSecure from '../../../Hooks/useAxiosSecure';
-import { useQuery } from '@tanstack/react-query';
+import React from "react";
+import { useParams } from "react-router";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 const ContestParticipants = () => {
     const { contestId } = useParams();
     const axiosSecure = useAxiosSecure();
 
-    const {data: contestParticipants = []} = useQuery({
-        queryKey: ['contestPaticipants', contestId],
+    const { data: contestParticipants = [], refetch } = useQuery({
+        queryKey: ["contestPaticipants", contestId],
         queryFn: async () => {
-            const res = await axiosSecure.get(`/contest-registrations/${contestId}`)
+            const res = await axiosSecure.get(`/contest-registrations/${contestId}`);
             return res.data;
-        }
-    })
+        },
+    });
+
+    const handleSelectWinner = async (participantEmail) => {
+        Swal.fire({
+            title: "Are you sure to select this participant as winner?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Select!",
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                try {
+                    // get the winner info by email
+                    const res = await axiosSecure.get(`/users/${participantEmail}`);
+                    const winnerInfo = {
+                        winnerName: res.data.displayName,
+                        winnerPhoto: res.data.photoURL,
+                        winnerEmail: participantEmail,
+                    };
+
+                    // update contest winner info into the database
+                    await axiosSecure.patch(`/contests/${contestId}`, { winnerInfo });
+                    Swal.fire({
+                        title: "🎉 Booyah! 🎉",
+                        text: "🎉 Winner has been selected! 🎉",
+                        icon: "success",
+                    });
+                    refetch();
+                } catch (error) {
+                    console.error("Error selecting winner:", error);
+                    Swal.fire("Error!", "Failed to select winner.", "error");
+                }
+            }
+        });
+    };
 
     return (
         <div className="p-4">
@@ -36,20 +73,14 @@ const ContestParticipants = () => {
                         {contestParticipants.map((participant, index) => (
                             <tr key={participant._id}>
                                 <th>{index + 1}</th>
-                                <td>
-                                    {
-                                        participant?.participantName ? participant.participantName : "Mohammad Rashel"
-                                    }
-                                </td>
+                                <td>{participant?.participantName ? participant.participantName : "Mohammad Rashel"}</td>
                                 <td>{participant.userEmail}</td>
                                 <td>{new Date(participant.submittedAt).toLocaleString()}</td>
+                                <td>{participant.submittedTask ? participant.submittedTask : "Not Submitted"}</td>
                                 <td>
-                                    {
-                                        participant.submittedTask ? participant.submittedTask : "Not Submitted"
-                                    }
-                                </td>
-                                <td>
-                                    <button className="btn btn-sm btn-primary">Select as Winner</button>
+                                    <button onClick={() => handleSelectWinner(participant.userEmail)} className="btn btn-sm btn-primary">
+                                        Select as Winner
+                                    </button>
                                 </td>
                             </tr>
                         ))}
